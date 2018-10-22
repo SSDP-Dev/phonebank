52bec886f5ecea0d7dbc25471ac08db84f84b4f9
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-22 12:11:21 -0600

    add nd calls file and array to leaderboard script

--- leaderboard-generate.php
+++ leaderboard-generate.php
@@ -7,9 +7,25 @@ $mi_calls_v1_file = '../raw-leaderboard-data/MichiganCallRecords102018.csv';
 $mi_calls_v2_file = '../raw-leaderboard-data/MichiganCalls.csv';
 $ut_calls_v1_file = '../raw-leaderboard-data/UtahCallRecords102018.csv';
 $ut_calls_v2_file = '../raw-leaderboard-data/UtahCalls.csv';
+$nd_calls_v1_file = "../raw-leaderboard-data/NDCalls.csv";
 
 // Section 2: load the files into arrays - we do this four times
 // Once per file.
+
+$nd_calls_v1_array = [];
+
+// Open the file for reading
+if (($h = fopen("{$nd_calls_v1_file}", "r")) !== false) {
+    // Each line in the file is converted into an individual array that we call $data
+    // The items of the array are comma separated
+    while (($data = fgetcsv($h, 1000, ",")) !== false) {
+        // Each individual array is being pushed into the nested array
+        $nd_calls_v1_array[] = $data;
+    }
+    // Close the file
+    fclose($h);
+}
+
 // The nested array to hold all the arrays
 $mi_calls_v1_array = [];
 
@@ -71,7 +87,7 @@ if (($h = fopen("{$ut_calls_v2_file}", "r")) !== false) {
 
 
 // Section 3: Merge all the arrays
-$final_array = array_merge($mi_calls_v1_array, $mi_calls_v2_array, $ut_calls_v1_array, $ut_calls_v2_array);
+$final_array = array_merge($mi_calls_v1_array, $mi_calls_v2_array, $ut_calls_v1_array, $ut_calls_v2_array, $nd_calls_v1_array);
 
 // var_dump($final_array[1][6]);
 
