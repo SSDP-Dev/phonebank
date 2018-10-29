723c772538d4d72151851a9ac062269e532b547e
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-29 16:44:29 -0600

    add missouri to leaderboard php

--- leaderboard-generate.php
+++ leaderboard-generate.php
@@ -8,6 +8,7 @@ $mi_calls_v2_file = '../raw-leaderboard-data/MichiganCalls.csv';
 $ut_calls_v1_file = '../raw-leaderboard-data/UtahCallRecords102018.csv';
 $ut_calls_v2_file = '../raw-leaderboard-data/UtahCalls.csv';
 $nd_calls_v1_file = "../raw-leaderboard-data/NDCalls.csv";
+$mo_calls_v1_file = "../raw-leaderboard-data/MissouriCalls.csv";
 
 // Section 2: load the files into arrays - we do this four times
 // Once per file.
@@ -85,9 +86,23 @@ if (($h = fopen("{$ut_calls_v2_file}", "r")) !== false) {
     fclose($h);
 }
 
+$mo_calls_v1_array = [];
+
+// Open the file for reading
+if (($h = fopen("{$mo_calls_v1_file}", "r")) !== false) {
+    // Each line in the file is converted into an individual array that we call $data
+    // The items of the array are comma separated
+    while (($data = fgetcsv($h, 1000, ",")) !== false) {
+        // Each individual array is being pushed into the nested array
+        $mo_calls_v1_array[] = $data;
+    }
+    // Close the file
+    fclose($h);
+}
+
 
 // Section 3: Merge all the arrays
-$final_array = array_merge($mi_calls_v1_array, $mi_calls_v2_array, $ut_calls_v1_array, $ut_calls_v2_array, $nd_calls_v1_array);
+$final_array = array_merge($mi_calls_v1_array, $mi_calls_v2_array, $ut_calls_v1_array, $ut_calls_v2_array, $nd_calls_v1_array, $mo_calls_v1_array);
 
 // var_dump($final_array[1][6]);
 
